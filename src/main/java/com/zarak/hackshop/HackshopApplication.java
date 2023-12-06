2cdf3418e18ca9fa808caee8179492b33b749717
Kehan Zhao <zhao7720@163.com>
2023-12-06 19:51:49 +0800

    init

---
+++ src/main/java/com/zarak/hackshop/HackshopApplication.java
@@ -0,0 +1,13 @@
+package com.zarak.hackshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zarak/hackshop/HackshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarak.hackshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
